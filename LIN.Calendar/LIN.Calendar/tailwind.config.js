e01f361903a21395e139d9da10aa8c8d1ee7d5a0
Alex <giraldojhong4@gmail.com>
2024-04-21 02:31:20 -0500

    Mejoras de UI

--- LIN.Calendar/LIN.Calendar/tailwind.config.js
+++ LIN.Calendar/LIN.Calendar/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["../**/*{html,razor,js,cs}"],
-  theme: {
-    extend: {},
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'dl': '910px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+      extend: {
+      },
   },
   plugins: [],
 }
