3e6c820b293784ce7477f9b91bc05d597037f26f
Alex <giraldojhong4@gmail.com>
2024-04-21 23:15:58 -0500

    Corrección de compatibilidad

--- LIN.Calendar/LIN.Calendar/tailwind.config.js
+++ LIN.Calendar/LIN.Calendar/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../**/*{html,razor,js,cs}", "D:/LIN Services/Components/LIN.Contacts.Shared/**/*{html,razor,js,cs}"],
+    content: ["../**/*{html,razor,js,cs}", "D:/LIN Services/Components/LIN.Calendar.Shared/**/*{html,razor,js,cs}"],
     theme: {
         screens: {
             'sm': '640px',
